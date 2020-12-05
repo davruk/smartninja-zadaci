44ea355e91870434f4c9778cfcaa04297e532608
Davor Rukavina <revan.mac@gmail.com>
2020-12-05 20:31:11 +0100

    Add files via upload

---
+++ guess the secret number.py
@@ -0,0 +1,11 @@
+secret = 22
+
+"guess" - int(input("Guess the secret number (between 1 and 30):"))
+
+if "guess" == secret:
+    print("You've guessed it correctly - It is the number 22.")
+else:
+    print("Sorry, your guess is not correct. Try again" + str("guess"))
+
+
+
